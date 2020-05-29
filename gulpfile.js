4b9a02830f22682df3c32103648e674cf55a2a3a
Alex <alexlada86@gmail.com>
2020-05-29 23:54:25 +0300

    add header style

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
-  return gulp.src('./projects/005.barbershop_2.0/sass/**/*.scss')
+  //return gulp.src('./projects/005.barbershop_2.0/sass/**/*.scss')
+  return gulp.src('./projects/005.barbershop_2.0/sass/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./projects/005.barbershop_2.0/css'));
 });
