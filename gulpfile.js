fccbf768a380c54a804b301127536fd161642fb7
Alex <alexlada86@gmail.com>
2020-06-06 03:31:48 +0300

    end header

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,8 @@ sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
   //return gulp.src('./projects/005.barbershop_2.0/sass/**/*.scss')
-  return gulp.src('./projects/005.barbershop_2.0/sass/style.scss')
+  return gulp.src('./projects/006.Pink/sass/style.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./projects/005.barbershop_2.0/css'));
+    .pipe(gulp.dest('./projects/006.Pink/css'));
 });
 
